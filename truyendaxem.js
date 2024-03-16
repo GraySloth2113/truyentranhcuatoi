a49eb561fb79da7aad40d6a0f447612c61f6af2d
GraySloth2113 <vinhdragonkun@gmail.com>
2024-03-17 05:19:14 +0700

    truyendaxem

---
+++ truyendaxem.js
@@ -0,0 +1,19 @@
+let theloai = document.getElementsByClassName("list")[1];
+let select1 = document.querySelector(".select_theloai");
+
+theloai.addEventListener("mouseover",()=>{
+    select1.style.display="block";
+    
+})
+theloai.addEventListener("mouseout",()=>{
+    select1.style.display="none";
+    
+})
+select1.addEventListener("mouseover",()=>{
+    select1.style.display="block";
+    
+})
+select1.addEventListener("mouseout",()=>{
+    select1.style.display="none";
+    
+})
